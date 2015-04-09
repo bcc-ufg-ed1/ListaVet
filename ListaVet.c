71a96748c1ea74f56cd85a00cfe6032b1d361594
yago franco ribeiro <yagofranco11@hotmail.com>
2015-04-09 00:50:07 -0300

    LISTA
    Fiz todas implementaçoes !!!

--- ListaVet.c
+++ ListaVet.c
@@ -51,7 +51,8 @@ int inserir(ListaVet* lista, int item, int pos) {
         return INDICE_INVALIDO;
 
     // Desloca elementos para a direita.
-    for(int i = lista->ultimo; i > pos; i--)  {
+    int i;
+    for(i = lista->ultimo; i > pos; i--)  {
         lista->itens[i] = lista->itens[i - 1];
     }
     lista->itens[pos] = item;
@@ -70,7 +71,8 @@ int remover(ListaVet* lista, int* item, int pos) {
     *item = lista->itens[pos];
 
     // Desloca elementos para a esquerda.
-    for(int i = pos; i < (lista->ultimo - 1); i++)  {
+    int i;
+    for(i = pos; i < (lista->ultimo - 1); i++)  {
         lista->itens[i] = lista->itens[i + 1];
     }
     lista->ultimo--;
@@ -86,6 +88,7 @@ int obterElemento(ListaVet* lista, int* item, int pos) {
         return INDICE_INVALIDO;
 
     *item = lista->itens[pos];
+
     return OK;
 }
 
@@ -96,3 +99,104 @@ int obterTamanho(ListaVet* lista, int* tam) {
     return OK;
 }
 
+int obterPosicao(ListaVet* lista, int* pos, int elemento){
+    if (lista == NULL)
+        return ESTRUTURA_NAO_INICIALIZADA;
+    if (estahVazia(lista))
+        return ESTRUTURA_VAZIA;
+
+    int i;
+        for(i=0;i<lista->ultimo;i++){
+
+            if(elemento== lista->itens[i]){
+                    elemento=i;
+                    *pos = elemento;
+                    }
+                }
+        if (elemento!=*pos)
+           return -1;
+
+return OK;
+}
+
+int removerInicio(ListaVet* lista, int* item) {
+    int pos = 0;
+    if (lista == NULL)
+        return ESTRUTURA_NAO_INICIALIZADA;
+    if (estahVazia(lista))
+        return ESTRUTURA_VAZIA;
+    if (!verificaIndice(lista, pos, FALSE))
+        return INDICE_INVALIDO;
+
+    *item = lista->itens[pos];
+
+    // Desloca elementos para a esquerda.
+    int i;
+    for(i = pos; i < (lista->ultimo - 1); i++)  {
+        lista->itens[i] = lista->itens[i + 1];
+    }
+    lista->ultimo--;
+    return OK;
+}
+
+int removerFinal(ListaVet* lista, int* item) {
+    int pos = 0;
+    if (lista == NULL)
+        return ESTRUTURA_NAO_INICIALIZADA;
+    if (estahVazia(lista))
+        return ESTRUTURA_VAZIA;
+    if (!verificaIndice(lista, pos, FALSE))
+        return INDICE_INVALIDO;
+
+    pos = lista->ultimo - 1;
+    *item = lista->itens[pos];
+
+    // Desloca elementos para a esquerda.
+    int i;
+    for(i = pos; i < (lista->ultimo - 1); i++)  {
+        lista->itens[i] = lista->itens[i + 1];
+    }
+    lista->ultimo--;
+    return OK;
+}
+
+int inserirInicio(ListaVet* lista, int item) {
+    int pos = 0;
+    if (lista == NULL)
+        return ESTRUTURA_NAO_INICIALIZADA;
+    if (estahCheia(lista))
+        return ESTRUTURA_CHEIA;
+    if (!verificaIndice(lista, pos, TRUE))
+        return INDICE_INVALIDO;
+
+    // Desloca elementos para a direita.
+    int i;
+    for(i = lista->ultimo; i > pos; i--)  {
+        lista->itens[i] = lista->itens[i - 1];
+    }
+    lista->itens[pos] = item;
+    lista->ultimo++;
+    return OK;
+}
+
+int inserirFim(ListaVet* lista, int item) {
+    int pos = lista->ultimo -1;
+    if (lista == NULL)
+        return ESTRUTURA_NAO_INICIALIZADA;
+    if (estahCheia(lista))
+        return ESTRUTURA_CHEIA;
+    if (!verificaIndice(lista, pos, TRUE))
+        return INDICE_INVALIDO;
+
+
+    pos = lista->ultimo;
+    // Desloca elementos para a direita.
+    int i;
+
+    for(i = lista->ultimo; i > pos; i--)  {
+        lista->itens[i] = lista->itens[i - 1];
+    }
+    lista->itens[pos] = item;
+    lista->ultimo++;
+    return OK;
+}
